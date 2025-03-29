6805fd5c73c135b1b73fcd3a37310321e71805fd
LocCongNghe <117754394+LocCongNghe@users.noreply.github.com>
2025-03-30 03:27:00 +0700

    abc

---
+++ layers.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    def call(self, input_embedding, validation_embedding):
+        # Lấy phần tử đầu tiên trong list (vì nó chứa Tensor)
+        input_embedding = input_embedding[0] if isinstance(input_embedding, list) else input_embedding
+        validation_embedding = validation_embedding[0] if isinstance(validation_embedding, list) else validation_embedding
+        
+        return tf.math.abs(input_embedding - validation_embedding)
